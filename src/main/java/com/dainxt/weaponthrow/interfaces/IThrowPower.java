87a295d95cf67ab859bf90adc8f8950767ababd1
dainxt <68045253+dainxt@users.noreply.github.com>
2022-12-25 04:32:05 -0500

    Delete src/main directory

--- src/main/java/com/dainxt/weaponthrow/interfaces/IThrowPower.java
+++
@@ -1,8 +0,0 @@
-package com.dainxt.weaponthrow.interfaces;
-
-public interface IThrowPower {
-	public int getChargeTime();
-	public void setChargeTime(int ticks);
-	public boolean doesMaxChargeChanged();
-	public void tick();
-}
